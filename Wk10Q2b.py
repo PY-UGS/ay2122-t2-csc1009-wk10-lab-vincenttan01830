c6cc465d1f4bf431aa2a5dfd3c58d36311916507
vincenttan01830 <39579297+vincenttan01830@users.noreply.github.com>
2022-04-05 09:58:49 +0800

    Add files via upload

---
+++ Wk10Q2b.py
@@ -0,0 +1,11 @@
+switcher = {
+    "CSC1006": "Mathematics 2",
+    "CSC1007": "Operating Systems",
+    "CSC1008": "Data Structures and Algorithms",
+    "CSC1009": "Object-Oriented Programming",
+    "CSC1010": "Computer Networks"
+}
+
+#output
+module = "CSC1009"
+print(switcher.get(module))
